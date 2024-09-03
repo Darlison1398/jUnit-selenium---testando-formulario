ad67e148373214e7e8c70eec3e717f95e4c832e8
Darlison1398 <drsilva123jd@gmail.com>
2024-09-02 23:34:22 -0300

    Nova atualização

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,6 +12,8 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+
+import static org.junit.Assert.assertTrue;
 
 public class TestMain {
 
@@ -65,19 +68,63 @@ public class TestMain {
     }
 
 
-//    @Test
-//    public void test_button_attention(){
-//        driver.findElement(By.id("btn-attt")).click();
-//
-//        driver.switchTo().window("modal-open");
-////        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-////        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("modal-open")));
-//
-//        driver.findElement(By.className("btn.btn-outline-dark")).click();
-//
-//        //driver.switchTo().window("modal-open");
-//        //WebElement closeButton = wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-close")));
-//        //closeButton.click();
-//       // driver.getWindowHandle();
-//    }
+    @Test
+    public void test_button_attention(){
+        driver.findElement(By.id("btn-attt")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("box-modal-att")));
+
+        WebElement header = driver.findElement(By.className("modal-header"));
+        WebElement btnClose = header.findElement(By.className("btn-close"));
+        btnClose.click();
+
+    }
+
+
+    @Test
+    public void test_button_aviso(){
+        driver.findElement(By.id("btn-attt-a")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("box-modal-att-a")));
+
+        // Usa um seletor CSS para encontrar o botão de fechar
+        WebElement btnClose = driver.findElement(By.cssSelector("#box-modal-att-a .btn.btn-outline-dark"));
+
+        // Espera até que o botão esteja clicável e então o clica
+        wait.until(ExpectedConditions.elementToBeClickable(btnClose)).click();
+
+    }
+
+
+    @Test
+    public void test_button_codigos(){
+        WebElement button = driver.findElement(By.cssSelector("button.btn.btn-primary[data-bs-toggle='offcanvas'][data-bs-target='#demo']"));
+        button.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebElement offcanvas = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("demo")));
+
+        assertTrue(offcanvas.getAttribute("class").contains("show"));
+
+        WebElement header = offcanvas.findElement(By.className("offcanvas-header"));
+        WebElement close = header.findElement(By.cssSelector(".btn-close"));
+        close.click();
+
+    }
+
+
+    @Test
+    public void test_button_mais() {
+        driver.findElement(By.id("btn-mais")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("demo-box")));
+
+        //driver.findElement(By.id("page")).click();
+
+        WebElement header = driver.findElement(By.className("offcanvas-header"));
+        WebElement closeBtn = header.findElement(By.className("btn-close"));
+        closeBtn.click();
+
+    }
 }
